c851f39f709b85ea59c12ec930c79227e38e66ce
saurav23 <sauravtarun813@gmail.com>
2020-12-06 16:31:23 +0530

    Day 3

--- 2. Section-2/basics-01-starting-project/assets/scripts/app.js
+++ 2. Section-2/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert("this will work !")
+// alert("this will work !")
+let currentResult = 0 ;
+
+currentResult = currentResult + 10 ;
+
+let calculationDescription = '( ' + currentResult + ' + 10 )' ; // string concatenation
+
+outputResult(currentResult, calculationDescription);
