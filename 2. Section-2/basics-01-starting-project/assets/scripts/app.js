b0dc8a655cce468e12c1d4bacb04aaf48f0bcbf8
saurav23 <sauravtarun813@gmail.com>
2020-12-07 02:19:23 +0530

    Day 4

--- 2. Section-2/basics-01-starting-project/assets/scripts/app.js
+++ 2. Section-2/basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,15 @@
-// alert("this will work !")
+function add(num1, num2){
+    const result = num1 + num2 ;
+    // alert ('The result is ' + result ) ;
+    return result ;
+}
+// ----------------------------------------------------------------------------------------------
 const defaultResult = 0 ;
 let currentResult = defaultResult ;
+// currentResult = currentResult + 20 ;
 
-currentResult = currentResult + 10 ;
+currentResult = add(2,6);
+
 
 let calculationDescription = '( ' + defaultResult + ' + 10 )' ; // string concatenation
 
