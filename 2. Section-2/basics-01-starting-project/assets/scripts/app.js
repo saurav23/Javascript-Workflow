b75fc91370e5dfe797e36aca2e77964b8b843862
saurav23 <sauravtarun813@gmail.com>
2020-12-07 21:38:57 +0530

    Day 4

--- 2. Section-2/basics-01-starting-project/assets/scripts/app.js
+++ 2. Section-2/basics-01-starting-project/assets/scripts/app.js
@@ -1,16 +1,45 @@
-function add(num1, num2){
-    const result = num1 + num2 ;
-    // alert ('The result is ' + result ) ;
-    return result ;
+function getUserInput(){
+    return parseInt(userInput.value) ;
 }
+
+function createAndWriteOutput(operator, resultBeforeCalculation, calculationNumber){
+    const calcDescription = `${resultBeforeCalculation} ${operator} ${calculationNumber}`;
+    outputResult(currentResult,calcDescription);
+}
+
+function add(){
+    const enteredNumber = getUserInput() ;
+    const initialResult = currentResult ;
+    currentResult = currentResult + enteredNumber ;
+    createAndWriteOutput('+', initialResult, enteredNumber) ;
+}
+
+function subtract(){
+    const enteredNumber = getUserInput() ;
+    const initialResult = currentResult ;
+    currentResult = currentResult - enteredNumber ;
+    createAndWriteOutput('-', initialResult, enteredNumber) ;
+}
+
+function multiply(){
+    const enteredNumber = getUserInput() ;
+    const initialResult = currentResult ;
+    currentResult = currentResult * enteredNumber ;
+    createAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide(){
+    const enteredNumber = getUserInput() ;
+    const initialResult = currentResult ;
+    currentResult = currentResult / enteredNumber ;
+    createAndWriteOutput('/', initialResult, enteredNumber) ;
+}
+
 // ----------------------------------------------------------------------------------------------
 const defaultResult = 0 ;
 let currentResult = defaultResult ;
-// currentResult = currentResult + 20 ;
 
-currentResult = add(2,6);
-
-
-let calculationDescription = '( ' + defaultResult + ' + 10 )' ; // string concatenation
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
